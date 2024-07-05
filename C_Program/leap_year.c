f2b9777b429646723c03f4f89866a18e3cfc4f38
muskan <muskan8878h@gmail.com>
2024-07-05 16:41:35 +0530

    check lead year

---
+++ C_Program/leap_year.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n%400==0 || (n%4==0 && n%100!=0)){
+        printf("%d is leap year",n);
+    }
+    else{
+        printf("%d is not leap year",n);
+    }
+}
